fe453dd4fafcaac8c717084edb75bcfbb741de8f
Brian Anderson <banderson623@gmail.com>
2012-09-05 00:08:12 -0500

    added test code

--- tests/IntervalTest.java
+++ tests/IntervalTest.java
@@ -1,3 +1,8 @@
+import java.security.InvalidParameterException;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created with IntelliJ IDEA.
  * User: brian_anderson
@@ -6,13 +11,51 @@
  * To change this template use File | Settings | File Templates.
  */
 public class IntervalTest implements IScheduler.IInterval {
+    int m_start, m_end;
+
+    /**
+     * This method will generate a set of intervals of specified size with random
+     * start and end dates based off of today's date.
+     *
+     * @param length is the number of intervals to generate
+     * @return a java.util.Set of intervals
+     */
+    static Set<IntervalTest> BuildSetOfLength(int length){
+        Set<IntervalTest> theSet =new HashSet<IntervalTest>(length);
+
+        for(int i = 0; i < length; ++i){
+            theSet.add(new IntervalTest());
+        }
+
+        return theSet;
+    }
+
+    IntervalTest(){
+        Date now = new Date();
+        Long longTime = new Long(now.getTime()/1000);
+
+        m_start =  (int) (longTime + Math.round(Math.random() * 10000));
+        m_end = m_start + 1000 + (int) Math.round(Math.random() * 1000);
+    }
+
+    // Simple constructor
+    IntervalTest(int start, int end){
+        if(end < start){
+            throw new InvalidParameterException("Start must be before end");
+        }
+
+        m_start = start;
+        m_end = end;
+        System.out.println("Creating Interval m_start: " + m_start + ", m_end: " + m_end);
+    }
+
     @Override
     public int getStartTime() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return m_start;
     }
 
     @Override
     public int getEndTime() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return m_end;
     }
 }
