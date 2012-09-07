b7a0f61c7b28b76e78eb06512e2cec7346ebfea7
Brian Anderson <banderson623@gmail.com>
2012-09-07 09:59:56 -0500

    Added assignment 2

---
+++ Project2/src/ArrayLookup.java
@@ -0,0 +1,43 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brian_anderson
+ * Date: 9/7/12
+ *
+ * This is for Fall 2012 Com S 311, Assignment #2 ...
+ *
+ * Implement three simple algorithms and perform experiments to empirically
+ * evaluate the running time of your implementations.
+ */
+public class ArrayLookup implements IArrayLookup {
+
+    /**
+     * Returns the value associated with the key 'key'.
+     *
+     * @param arr An array containing precisely one element with key value 'key'.
+     *            This array need not be sorted.
+     * @return
+     */
+    @Override
+    public Object linearLookup(Pair[] arr, Comparable key) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Pair[] selectionSort(Pair[] arr) {
+        return new Pair[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    /**
+     * Returns the value associated with the key 'key'.
+     *
+     * @param arr An array containing precisely one element with key value 'key'.
+     *            This array is sorted according to the ordering defined by T's implementation
+     *            of Comparable.
+     * @param key
+     * @return
+     */
+    @Override
+    public Object logLookup(Pair[] arr, Comparable key) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ Project2/src/IArrayLookup.java
@@ -0,0 +1,50 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brian_anderson
+ * Date: 9/7/12
+ * Time: 9:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IArrayLookup<T extends Comparable<? super T>, E>  {
+
+    //arr need not be sorted.  Method should take O(n) time.
+    /**
+     * Returns the value associated with the key 'key'.
+     * @param arr
+     * An array containing precisely one element with key value 'key'.
+     * This array need not be sorted.
+     * @return
+     */
+    public E linearLookup(Pair<T, E>[] arr, T key);
+
+    //Method should take O(n^2) time.
+    public Pair<T, E>[] selectionSort(Pair<T, E>[] arr);
+
+    /**
+     * Returns the value associated with the key 'key'.
+     * @param arr
+     * An array containing precisely one element with key value 'key'.
+     * This array is sorted according to the ordering defined by T's implementation
+     * of Comparable.
+     * @param key
+     * @return
+     */
+    //arr must be sorted.  Method should take O(log(n)) time.
+    public E logLookup(Pair<T, E>[] arr, T key);
+
+    public class Pair<K extends Comparable<? super K>, V> implements Comparable<Pair<K, V>>
+    {
+        public K key;
+        public V value;
+
+        public Pair(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(Pair<K, V> arg0) {
+            return key.compareTo(arg0.key);
+        }
+    }
+}
