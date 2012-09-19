1df8184234a7019263f1c8bd703ee2b9e4232658
Brian Anderson <banderson623@gmail.com>
2012-09-19 00:59:40 -0500

    Look ups and sorting working

--- Project2/test/main.java
+++ Project2/test/main.java
@@ -9,32 +9,53 @@
  */
 public class main {
 
+    public static void printPairs(IArrayLookup.Pair pairs[]){
+        for(int i = 0; i < pairs.length; ++i){
+            System.out.print("["+i+"]"+pairs[i] + ", ");
+            if(i%10 == 0){
+                System.out.print("\n");
+            }
+        }
+        System.out.println("");
+    }
+
     public static void main(String [ ] args){
 
         Builders builder = new Builders();
-        Timings timers = new Timings();
+//        Timings timers = new Timings();
 
-        System.out.println(timers);
+//        System.out.println(timers);
 
 //        timers.start("The whole thing");
         ArrayLookup lookerUpper = new ArrayLookup();
 
-        timers.start("Creating Array");
-            IArrayLookup.Pair pairs[] = builder.arrayOfPairsSizeWithGuarenteedKey(1000,"Ralph");
-        timers.stop();
+//        timers.start("Creating Array");
+            IArrayLookup.Pair pairs[] = builder.arrayOfPairsSizeWithGuarenteedKey(2000,"Ralph");
+//        timers.stop();
 
-        timers.start("Linear look up of 1,000");
+
+        //printPairs(pairs);
+        IArrayLookup.Pair sortedPairs[] = lookerUpper.selectionSort(pairs);
+//        printPairs(sortedPairs);
+
+        System.out.println("Finding");
+        Object value = lookerUpper.logLookup(sortedPairs, "Ralph");
+
+        System.out.println("Found: " + value);
+
+
+//        timers.start("Linear look up of 1,000");
             lookerUpper.linearLookup(pairs,"Ralph");
-        timers.stop();
+//        timers.stop();
 
         IArrayLookup.Pair pairs2[] = builder.arrayOfPairsSizeWithGuarenteedKey(10000,"Brian");
-        timers.start("Linear look up of 10,000");
+//        timers.start("Linear look up of 10,000");
         lookerUpper.linearLookup(pairs2,"Ralph");
-        timers.stop();
+//        timers.stop();
 
 
 //        timers.stop();
-        System.out.println(timers);
+//        System.out.println(timers);
     }
 
 }
