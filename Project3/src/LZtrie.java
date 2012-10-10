eb97263839e3a257e608aee000be5f0b3c532452
Brian Anderson <banderson623@gmail.com>
2012-10-10 01:04:33 -0500

    basic trie ideas

--- Project3/src/LZtrie.java
+++ Project3/src/LZtrie.java
@@ -4,6 +4,10 @@
  * Time: 12:50 AM
  * You are to implement the following interface for compressing data using the
  * LZ algorithm using a trie as described above, and a second time using the Java HashMap data structure.
+ *
+ * Sources Referenced while implementing this:
+ *  - https://forums.oracle.com/forums/thread.jspa?messageID=8787521
+ *
  */
 public class LZtrie implements ILZ {
 
@@ -11,4 +15,20 @@ public class LZtrie implements ILZ {
     public String encode(String uncompressed) {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
+
+    public class Trie {
+        private TrieNode rootNode;
+
+        /** Just to get things started */
+        public Trie(){
+            rootNode = new TrieNode();
+        }
+
+        public class TrieNode {
+
+        }
+
+    }
+
+
 }
