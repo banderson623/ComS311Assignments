2fecad3970ff8ce0e54be49f5543830d466139b0
Brian Anderson <banderson623@gmail.com>
2012-10-10 17:29:54 -0500

    Trie functioning, logging too

--- Project3/src/LZtrie.java
+++ Project3/src/LZtrie.java
@@ -5,7 +5,8 @@
  * You are to implement the following interface for compressing data using the
  * LZ algorithm using a trie as described above, and a second time using the Java HashMap data structure.
  *
- * Sources Referenced while implementing this:
+ * Sources Referenced while implementing this algorithm:
+ *  - http://en.wikipedia.org/wiki/Trie
  *  - https://forums.oracle.com/forums/thread.jspa?messageID=8787521
  *
  */
@@ -16,25 +17,38 @@ public class LZtrie implements ILZ {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
+    /**
+     * Todo: Remove this before submission, just for testing
+     */
+    public Trie trieFactory(){
+        return new Trie();
+    }
+
     public class Trie {
         private TrieNode rootNode;
 
         /** Just to get things started */
         public Trie(){
+            Log l = new Log("Trie::Trie");
+            l.log("Instantiated");
             rootNode = new TrieNode();
         }
 
         public boolean addString(String onlyOfOnesAndZeros){
+            Log l = new Log("Trie::addString");
+            l.log("Adding String \"" + onlyOfOnesAndZeros + "\"");
             return rootNode.addString(onlyOfOnesAndZeros);
         }
 
         public class TrieNode {
-            private TrieNode parent;
+            //private TrieNode parent;
             private TrieNode left;
             private TrieNode right;
 
             public TrieNode(){
-                parent =    null;
+                Log l = new Log("TrieNode::TrieNode");
+                l.log("Instantiated");
+                //parent =    null;
                 left =      null;
                 right =     null;
             }
@@ -47,7 +61,28 @@ public class LZtrie implements ILZ {
              * @return
              */
             public boolean addString(String onlyOfOnesAndZeros){
-                return true;
+                Log l = new Log("TrieNode::addString");
+                l.log(onlyOfOnesAndZeros);
+                if(onlyOfOnesAndZeros.length() > 0){
+                    char value = onlyOfOnesAndZeros.charAt(0);
+                    l.log("Adding/Checking '" + value + "'");
+                    if(value == '0'){
+                        // goes to left child
+                        l.log("left");
+                        if(left == null) left = new TrieNode();
+                        return left.addString(onlyOfOnesAndZeros.substring(1));
+                    } else if(value == '1') {
+                        // goes to right child
+                        l.log("right");
+                        if(right == null) right = new TrieNode();
+                        return right.addString(onlyOfOnesAndZeros.substring(1));
+                    } else {
+                        throw new InternalError("Only strings containing 0 and 1 are accepted " + onlyOfOnesAndZeros);
+                    }
+                } else {
+                    l.log("empty string");
+                    return false;
+                }
             }
         }
 
--- Project3/test/BasicTest.java
+++ Project3/test/BasicTest.java
@@ -6,5 +6,28 @@ public class BasicTest {
 
     public static void main(String [ ] args){
         System.out.println("Hello world, this is Assignment 3");
+        trieTest();
+        trieTestShouldError();
+
     }
+
+    public static void trieTest() {
+        LZtrie lzt = new LZtrie();
+        LZtrie.Trie t = lzt.trieFactory();
+        Log.off();
+        t.addString("00");
+    }
+
+    public static void trieTestShouldError() {
+        LZtrie lzt = new LZtrie();
+        LZtrie.Trie t = lzt.trieFactory();
+        try {
+            Log.off();
+            t.addString("a");
+            Log.resume();
+        } catch (InternalError e){
+            System.out.println("Got good error!");
+        }
+    }
+
 }
